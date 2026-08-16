9147f4dbefb8f0a5b4d1450ef6b8d3c79e5a778e
dc21v7n530 <nhandc21v7n530@vlvh.ctu.edu.vn>
2026-08-16 12:49:43 +0700

    Create h.sql

---
+++ h.sql
@@ -0,0 +1,4 @@
+INSERT INTO quotes (quote, source, favorite) VALUES
+('Thời gian không chờ một ai', 'Khuyết danh', false),
+('Học lập trình web thật thú vị!', 'Albert Einstein', true),
+('Yêu là chết trong lòng một ít!', 'Xuân Diệu', false);
